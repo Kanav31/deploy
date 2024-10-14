0fb6488f7fee4ba06ec9fd3b64870bacabad54fc
Kanav Agarwal <agarwalkanav3108@gmail.com>
2024-10-15 02:10:15 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get('/api/test1', (req, res) => {
+    res.json({ message: 'This is test endpoint 1' });
+});
+
+app.get('/api/test2', (req, res) => {
+    res.json({ message: 'This is test endpoint 2' });
+});
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
